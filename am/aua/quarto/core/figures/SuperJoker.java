6340ce27266eee667491c6e8c2247d41148f44d2
Hamayak-Musheghyan <162161585+Hamayak-Musheghyan@users.noreply.github.com>
2024-05-11 16:47:44 +0400

    Update SuperJoker.java

--- am/aua/quarto/core/figures/SuperJoker.java
+++ am/aua/quarto/core/figures/SuperJoker.java
@@ -8,6 +8,17 @@ public class SuperJoker extends SpecialFigure {
     public String toString(){
         return "++++";
     }
-
+    public boolean isSameColor(Figure other){
+        return  true;
+    }
+    public boolean isSameHeight(Figure other){
+        return true;
+    }
+    public boolean isSameShape(Figure other){
+        return true;
+    }
+    public boolean isSameForm(Figure other){
+        return  true;
+    }
 
 }
